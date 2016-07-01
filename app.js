deb8adf5e695f55f2e7af17684dffaecb99cf8ce
Jed Mendoza <jed.mendoza@gmail.com>
2016-07-01 15:45:11 -0700

    User's post now has live feed option #3

--- app.js
+++ app.js
@@ -8,25 +8,51 @@ app.use(jsonParser)
 
 app.use(express.static('./'));
 
-app.get('/', function(req, res) {
-  res.send('testing')
-});
-
+// Add a new post for a particular user.
 app.post('/posts/:user', function(req, res) {
-  posts.data.push(req.body);
+  // If the post already exists, replace it.
+  var match = false;
+  posts.data.forEach(function(post) {
+    if (req.body.id == post.id) {
+      post.entry = req.body.entry;
+      match = true;
+    }
+  });
+  // If the post doesn't already exist, add it.
+  if (!match) {
+    posts.data.push(req.body);
+  }
+
   res.send();
 });
 
+// Return posts that haven't yet expired.
 app.get('/posts/', function(req, res) {
+  // Get all the valid posts.
+  var valid = isValid();
+  res.json(valid);
+});
+
+// Return the lastest post that isn't expired for this user.
+app.get('/posts/recent/:user', function(req, res) {
+  var valid = isValid();
+
+  var last = valid[valid.length - 1];
+  res.json(last);
+});
+
+function isValid() {
   var valid = []
+
   posts.data.forEach(function(post) {
+    // If the post hasn't expired it will be returned.
     if ((post.time + 10000) > Date.now() || post.delete === 'false') {
       valid.push(post)
     }
-  })
-    res.json(valid);
-    console.log(valid)
-});
+  });
+
+  return valid;
+}
 
 // app.get('/posts/', function(req, res) {
 //   posts.data.forEach(function(entry) {})
--- default.js
+++ default.js
@@ -2,56 +2,121 @@ var theJournal = document.getElementById('journal');
 var submitButton = document.getElementById('post-button');
 var deleteButton = document.getElementById('destroy');
 var journalDiv = document.getElementById('journalDiv');
-// var entriesDiv = document.getElementById('previous-entries')
 
-// $('#journalModalDiv').modal('toggle')
+// All the user to edit the most recent post when the page loads.
+document.addEventListener('DOMContentLoaded', function() {
+  recent = new XMLHttpRequest();
+  recent.open('GET', '/posts/recent/jed')
+  recent.send()
 
+  recent.addEventListener('load', function() {
+    var journal = document.getElementById('journal');
+    if (!recent.responseText == '') {
+      // We have a journal entry.
+      var response = JSON.parse(recent.responseText);
+
+      if (response.user == 'undefined') {
+        console.log('No user found.')
+      } else {
+        journal.setAttribute('data-id', response.id);
+        journal.setAttribute('data-user', response.user);
+        journal.textContent = response.entry;
+      }
+
+      // TODO XXX(Jed): Add the post to the page.
+        // var livejournal = document.getElementById('livejournal');
+        //
+        // var entryDiv = document.createElement('div');
+        // entryDiv.setAttribute('class', 'panel panel-default');
+        //
+        // var entryBody = document.createElement('div');
+        // entryBody.setAttribute('class', 'panel-body');
+        //
+        // var journalEntry = document.createElement('p');
+        // journalEntry.textContent = response.entry;
+        //
+        // livejournal.appendChild(entryDiv);
+        // entryDiv.appendChild(entryBody);
+        // entryBody.appendChild(journalEntry);
+        makeEntry(response);
+    }
+  });
+});
+
+// On key press save the entry to the server.
 theJournal.addEventListener('keyup', function(e) {
+  var theJournal = document.getElementById('journal');
+  var id = theJournal.getAttribute('data-id');
+
   var thePost = {};
+  // Check if this post already has an idea.
+  if (!id) {
+    thePost.id = Date.now();
+  } else {
+    thePost.id = id;
+  }
+
   thePost.entry = theJournal.value;
   thePost.user = 'jed';
   thePost.time = Date.now();
+
   if (deleteButton.checked) {
     thePost.delete = 'true'
   } else {
     thePost.delete = 'false'
   }
+
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/posts/' + 'jed');
   xhr.setRequestHeader('content-type', 'application/json');
   xhr.send(JSON.stringify(thePost));
 
-  entries();
-
+  makeEntry(thePost);
 });
+function makeEntry(response) {
+  var livejournal = document.getElementById('livejournal');
+  clear(livejournal)
+  var entryDiv = document.createElement('div');
+  entryDiv.setAttribute('class', 'panel panel-default');
 
+  var entryBody = document.createElement('div');
+  entryBody.setAttribute('class', 'panel-body');
+
+  var journalEntry = document.createElement('p');
+  journalEntry.textContent = response.entry;
+
+  livejournal.appendChild(entryDiv);
+  entryDiv.appendChild(entryBody);
+  entryBody.appendChild(journalEntry);
+
+}
 function entries() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/live/');
-  xhr.send()
-
-  xhr.addEventListener('load', function() {
-    var response = JSON.parse(xhr.response)
-    console.log(response);
-    var area = document.getElementById('previous-entries');
-    clear(area);
-    console.log(response)
-    response.forEach(function(entry) {
-
-      var entryPanel = document.createElement('div');
-      entryPanel.setAttribute('class', 'panel panel-default');
-
-      var entryPanelContent = document.createElement('div');
-      entryPanelContent.setAttribute('class', 'panel-body');
-
-      var oldPost = document.createElement('p');
-      oldPost.textContent = entry.entry;
-
-      area.appendChild(entryPanel);
-      entryPanel.appendChild(entryPanelContent);
-      entryPanelContent.appendChild(oldPost);
-    })
-  })
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', '/live/');
+  // xhr.send()
+  //
+  // xhr.addEventListener('load', function() {
+  //   var response = JSON.parse(xhr.response)
+  //
+  //   var area = document.getElementById('previous-entries');
+  //   clear(area);
+  //
+  //   response.forEach(function(entry) {
+  //
+  //     var entryPanel = document.createElement('div');
+  //     entryPanel.setAttribute('class', 'panel panel-default');
+  //
+  //     var entryPanelContent = document.createElement('div');
+  //     entryPanelContent.setAttribute('class', 'panel-body');
+  //
+  //     var oldPost = document.createElement('p');
+  //     oldPost.textContent = entry.entry;
+  //
+  //     area.appendChild(entryPanel);
+  //     entryPanel.appendChild(entryPanelContent);
+  //     entryPanelContent.appendChild(oldPost);
+  //   })
+  // })
 }
 
 // var livePost = window.setInterval(getLive, 500);
@@ -67,10 +132,10 @@ function entries() {
 //   })
 // }
 
-// function clear(area) {
-//   while(area.firstChild) {
-//     area.removeChild(area.firstChild)
-//   }
-// }
+function clear(area) {
+  while(area.firstChild) {
+    area.removeChild(area.firstChild)
+  }
+}
 
 // window.setInterval(entries, 10000)
